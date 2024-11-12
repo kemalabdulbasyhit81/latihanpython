61358cef67d481baf527aac4db87fb48e7534fb5
kemalabdulbasyhit81 <indomart940@gmail.com>
2024-11-12 13:28:45 +0700

    algoritma

---
+++ soal6-7.py
@@ -0,0 +1,21 @@
+print('*'*48)
+print('\t\tPROGAM LINGKARAN')
+print('*'*48)
+
+jari_jari = int(input('masukan nilai jari-jari\t:'))
+
+luas = 22/7 * jari_jari * jari_jari
+keliling = 2 * 22/7 * jari_jari
+print('luas\t\t:',luas,'cm2')
+print('keliling\t\t',keliling,'cm2')
+print('\t\tPROGAM TABUNG')
+print("*"*48)
+
+r = int(input('masukan nilai jari jari\t;'))
+tinggi = int(input('masukan nilai tinggi\t:'))
+
+volume = 3.14 * r * r * tinggi 
+lp = 2 * 3.14
+AA = r + tinggi
+
+print('volume\t\t:', volume,'cm2')
